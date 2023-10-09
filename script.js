aefb7e7268bd8e7cd78c9e4d58821963f74d2137
frankMarcelino <frank.silva.ti@gmail.com>
2023-10-09 16:00:00 -0300

    fim da aula 06

---
+++ script.js
@@ -0,0 +1,45 @@
+let currentPageUrl = 'https://swapi.dev/api/people/'
+
+window.onload = async () => {
+   try {
+    await  loadCharacters(currentPageUrl);
+   }catch (error) {
+    console.log(error);
+    alert('Erro ao carregar cards!');
+   } 
+};
+
+async function loadCharacters(url) {
+    const mainContent = document.getElementById('imain-content')
+    mainContent.innerHTML = ''; //limpa resultados anteriores
+
+    try {
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        responseJson.results.forEach((character)=> {
+            const card = document.createElement("div")
+            card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/1.jpg')`
+            card.className = "cards"
+
+            const characterNameBg = document.createElement("div")
+            characterNameBg.className = 'character-name-bg' 
+
+            const characterName = document.createElement("span")
+            characterName.className = "characters-name"
+            characterName.innerText = `${character.name}`
+
+            characterNameBg.appendChild(characterName)
+            card.appendChild(characterNameBg)
+            mainContent.appendChild(card)
+        })
+
+        currentPageUrl = url
+
+    } catch(error) {
+        alert('erro ao carregar os personagens')
+        console.log(error)
+
+    }
+}
